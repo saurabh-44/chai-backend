6f0c70fe53017fe0691cd97ac4981e0c5b680280
Saurabh yadav <938saurabh@gmail.com>
2024-10-07 13:15:02 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
+/* The constant holds the name of your MongoDB database, which is "videotube". This value will be appended to your MongoDB connection string when connecting to the 
+database. */
 export const DB_NAME = "videotube"

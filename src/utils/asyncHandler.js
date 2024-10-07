6337e3a083cb6c92fe69450fc776ce2950eb1509
Saurabh yadav <938saurabh@gmail.com>
2024-10-07 19:11:10 +0530

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,14 @@
-const asyncHandler = (requestHandler) => {
+/* asyncHandler function is a middleware utility used in Express.js applications to simplify error handling for asynchronous route handlers 
+(functions that return promises).*/
+
+const asyncHandler = (requestHandler) => {    // higher-order function, it takes a function (requestHandler) as an argument and returns a new function (middleware).
+    // requestHandler represents the route handler or middleware that contains asynchronous code (e.g., API calls, database queries, etc.).
     return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err)) // This is used to handle the requestHandler function, which could return a promise (from an asynchronous operation).
+        /*If the requestHandler function succeeds, it simply resolves the promise.
+          If the requestHandler throws an error or the promise rejects, it will be caught by the catch() block.
+        catch((err) => next(err)): If there’s any error in the asynchronous code, the error is passed to next(), which is the standard way in Express to 
+        forward errors to error-handling middleware.*/
     }
 }
 

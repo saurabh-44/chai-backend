97d34105dd62b330f6dc7781497ca643858c6701
Saurabh yadav <938saurabh@gmail.com>
2024-10-07 13:21:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express"  // It provides methods and functionality to handle HTTP requests, routing, and middleware.
+import cors from "cors"        // A middleware that allows you to manage Cross-Origin Resource Sharing (CORS), which controls which domains are allowed to make requests to your backend.
+import cookieParser from "cookie-parser"  // A middleware is used to parse cookies sent by the client in HTTP requests, making it easy to access cookies in your server.
 
 const app = express()
 
 app.use(cors({                            
-    origin: process.env.CORS_ORIGIN,        // from where can accept request from frontend 
-    credentials: true
+    origin: process.env.CORS_ORIGIN, // Frontend URL or '*' (for all origins) or from where can accept request from frontend 
+    credentials: true                //  allows the server to accept requests that include credentials, such as cookies, authorization headers, or TLS client certificates.
 }))
 
-app.use(express.json({limit: "16kb"}))      // accept json file with limit
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit: "16kb"}))   // accept json file with limit
+app.use(express.urlencoded({extended: true, limit: "16kb"}))   // This middleware parses incoming requests with URL-encoded payloads (typically from HTML forms).
+app.use(express.static("public"))        // This middleware serves static files (like images, CSS files, JavaScript files) from the "public" directory.
+app.use(cookieParser())      // This middleware parses cookies attached to incoming client requests. This allows you to access cookies easily using req.cookies in your routes.
 
 
 //routes import

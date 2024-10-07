5d09fbcd017cd4c3820ef4a1883171d62db648f3
Saurabh yadav <938saurabh@gmail.com>
2024-10-07 13:12:08 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants.js";  // constant is imported from a constants.js file. It likely holds the name of the specific MongoDB database you'll be working with.
 
-
+/*process.env.MONGODB_URI: This is the MongoDB connection URI stored in your .env file. It includes the credentials (username, password) and the cluster URL.
+/${DB_NAME}: This adds the specific database name (defined in constants.js) to the connection string. MongoDB allows multiple databases in one cluster, 
+and here you're specifying which database you want to use.*/
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // Once the connection is successful, this line logs a message to the console, 
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
@@ -12,4 +14,5 @@ const connectDB = async () => {
     }
 }
 
-export default connectDB
+export default connectDB   // exports the connectDB function as the default export from this module. This allows other files (like your index.js file) to import 
+// and use this function to connect to MongoDB.
